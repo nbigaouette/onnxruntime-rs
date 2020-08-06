aec7066dfe8f17d5f623a6c95c7d3cea367732ee
Nicolas Bigaouette <nbigaouette@gmail.com>
2020-08-05 22:05:48 -0400

    Use 'ONNXRUNTIME_INSTALL_DIR' to find pre-installed library

--- onnxruntime-sys/build.rs
+++ onnxruntime-sys/build.rs
@@ -1,10 +1,17 @@
 use std::{env, path::PathBuf};
 
 fn main() {
+    let onnxruntime_install_dir: PathBuf = env::var("ONNXRUNTIME_INSTALL_DIR").unwrap().into();
+    let include_dir = onnxruntime_install_dir.join("include/onnxruntime/core/session");
+    let clang_arg = format!("-I{}", include_dir.display());
+
     // Tell cargo to tell rustc to link the system onnxruntime
     // shared library.
     println!("cargo:rustc-link-lib=onnxruntime");
-    println!("cargo:rustc-link-search=target/onnxruntime/lib");
+    println!(
+        "cargo:rustc-link-search={}/lib",
+        onnxruntime_install_dir.display()
+    );
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
     println!("cargo:rerun-if-changed=wrapper.h");
@@ -17,7 +24,7 @@ fn main() {
         // bindings for.
         .header("wrapper.h")
         // The current working directory is 'onnxruntime-sys'
-        .clang_arg("-I../target/onnxruntime/include/onnxruntime/core/session")
+        .clang_arg(clang_arg)
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
