58aa0ea2e7223c31d12718fcef6351c6296d1acc
Nicolas Bigaouette <nbigaouette@gmail.com>
2020-08-09 10:29:08 -0400

    Use input dimension instead of hard coding one, assert values

--- onnxruntime/examples/sample.rs
+++ onnxruntime/examples/sample.rs
@@ -23,16 +23,22 @@ fn run() -> Result<(), Error> {
         .with_number_threads(1)?
         .load_model_from_file("squeezenet.onnx")?;
 
+    let input0_shape: Vec<usize> = session.inputs[0].dimensions().collect();
+    let output0_shape: Vec<usize> = session.outputs[0].dimensions().collect();
+
+    assert_eq!(input0_shape, [1, 3, 224, 224]);
+    assert_eq!(output0_shape, [1, 1000, 1, 1]);
+
     // initialize input data with values in [0.0, 1.0]
     let n: u32 = session.inputs[0].dimensions.iter().product();
     let array = Array::linspace(0.0_f32, 1.0, n as usize)
-        .into_shape((1, 3, 224, 224))
+        .into_shape(input0_shape)
         .unwrap();
     let input_tensor_values = vec![array];
 
     let outputs = session.run(input_tensor_values)?;
 
-    assert_eq!(outputs[0].shape(), [1, 1000, 1, 1]);
+    assert_eq!(outputs[0].shape(), output0_shape.as_slice());
     for i in 0..5 {
         println!("Score for class [{}] =  {}", i, outputs[0][[0, i, 0, 0]]);
     }
