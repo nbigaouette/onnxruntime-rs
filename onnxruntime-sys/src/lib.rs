fc01b7528d37cae5dadfd45fc891821df870e519
Nicolas Bigaouette <nbigaouette@gmail.com>
2020-08-09 23:26:58 -0400

    Disable clippy on sys crate

--- onnxruntime-sys/src/lib.rs
+++ onnxruntime-sys/src/lib.rs
@@ -1,6 +1,11 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+// Disable clippy and `u128` not being FFI-safe (see #1)
+#![allow(clippy::all)]
+#![allow(improper_ctypes)]
+
+#[allow(clippy::all)]
 
 include!(concat!(
     env!("CARGO_MANIFEST_DIR"),
