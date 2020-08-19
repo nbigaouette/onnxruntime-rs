a7179c9c5c1831121c2e46fca3dd457f8ff3a616
Nicolas Bigaouette <nbigaouette@gmail.com>
2020-08-18 23:10:19 -0400

    Add GPT2

--- onnxruntime/src/download/language/machine_comprehension.rs
+++ onnxruntime/src/download/language/machine_comprehension.rs
@@ -31,6 +31,10 @@ pub enum MachineComprehension {
     ///
     /// Source: [https://github.com/onnx/models/tree/master/text/machine_comprehension/roberta](https://github.com/onnx/models/tree/master/text/machine_comprehension/roberta)
     RoBERTa(RoBERTa),
+    /// Large transformer-based language model that given a sequence of words within some text, predicts the next word.
+    ///
+    /// Source: [https://github.com/onnx/models/tree/master/text/machine_comprehension/gpt-2](https://github.com/onnx/models/tree/master/text/machine_comprehension/gpt-2)
+    GPT2(GPT2),
 }
 
 /// Large transformer-based model that predicts sentiment based on given input text.
@@ -50,12 +54,31 @@ pub enum RoBERTa {
     RoBERTaSequenceClassification,
 }
 
+/// Large transformer-based language model that given a sequence of words within some text, predicts the next word.
+///
+/// > Transformer-based language model for text generation.
+///
+/// Source: [https://github.com/onnx/models/tree/master/text/machine_comprehension/gpt-2](https://github.com/onnx/models/tree/master/text/machine_comprehension/gpt-2)
+///
+/// Variant downloaded: ONNX Version 1.6 with Opset Version 10.
+#[derive(Debug, Clone)]
+pub enum GPT2 {
+    /// Pure GPT2
+    GPT2,
+    /// GPT2 + script changes
+    ///
+    /// See [https://github.com/onnx/models/blob/master/text/machine_comprehension/gpt-2/dependencies/GPT2-export.py](https://github.com/onnx/models/blob/master/text/machine_comprehension/gpt-2/dependencies/GPT2-export.py)
+    /// for the script changes.
+    GPT2LmHead,
+}
+
 impl ModelUrl for MachineComprehension {
     fn fetch_url(&self) -> &'static str {
         match self {
             MachineComprehension::BiDAF => "https://github.com/onnx/models/raw/master/text/machine_comprehension/bidirectional_attention_flow/model/bidaf-9.onnx",
             MachineComprehension::BERTSquad => "https://github.com/onnx/models/raw/master/text/machine_comprehension/bert-squad/model/bertsquad-10.onnx",
             MachineComprehension::RoBERTa(variant) => variant.fetch_url(),
+            MachineComprehension::GPT2(variant) => variant.fetch_url(),
         }
     }
 }
@@ -69,8 +92,33 @@ impl ModelUrl for RoBERTa {
     }
 }
 
+impl ModelUrl for GPT2 {
+    fn fetch_url(&self) -> &'static str {
+        match self {
+            GPT2::GPT2 => "https://github.com/onnx/models/raw/master/text/machine_comprehension/gpt-2/model/gpt2-10.onnx",
+            GPT2::GPT2LmHead => "https://github.com/onnx/models/raw/master/text/machine_comprehension/gpt-2/model/gpt2-lm-head-10.onnx",
+        }
+    }
+}
+
 impl From<MachineComprehension> for AvailableOnnxModel {
     fn from(model: MachineComprehension) -> Self {
         AvailableOnnxModel::Language(Language::MachineComprehension(model))
+    }
+}
+
+impl From<RoBERTa> for AvailableOnnxModel {
+    fn from(model: RoBERTa) -> Self {
+        AvailableOnnxModel::Language(Language::MachineComprehension(
+            MachineComprehension::RoBERTa(model),
+        ))
+    }
+}
+
+impl From<GPT2> for AvailableOnnxModel {
+    fn from(model: GPT2) -> Self {
+        AvailableOnnxModel::Language(Language::MachineComprehension(MachineComprehension::GPT2(
+            model,
+        )))
     }
 }
