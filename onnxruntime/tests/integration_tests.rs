26fed333c11fbbf7a60654ac0d9c8d7ff15f638d
Nicolas Bigaouette <nbigaouette@gmail.com>
2020-08-24 20:37:40 -0400

    Add MNIST integration test for number 5

--- onnxruntime/tests/integration_tests.rs
+++ onnxruntime/tests/integration_tests.rs
@@ -122,6 +122,80 @@ mod download {
         //     );
         // }
     }
+
+    #[test]
+    fn mnist_5() {
+        const IMAGE_TO_LOAD: &str = "mnist_5.jpg";
+
+        let environment = Environment::builder()
+            .with_name("test")
+            .with_log_level(LoggingLevel::Verbose)
+            .build()
+            .unwrap();
+
+        let mut session = environment
+            .new_session_builder()
+            .unwrap()
+            .with_optimization_level(GraphOptimizationLevel::Basic)
+            .unwrap()
+            .with_number_threads(1)
+            .unwrap()
+            .with_model_downloaded(DomainBasedImageClassification::Mnist)
+            .expect("Could not download model from file");
+
+        let input0_shape: Vec<usize> = session.inputs[0].dimensions().collect();
+        let output0_shape: Vec<usize> = session.outputs[0].dimensions().collect();
+
+        assert_eq!(input0_shape, [1, 1, 28, 28]);
+        assert_eq!(output0_shape, [1, 10]);
+
+        // Load image and resize to model's shape, converting to RGB format
+        let image_buffer: ImageBuffer<Luma<u8>, Vec<u8>> = image::open(
+            Path::new(env!("CARGO_MANIFEST_DIR"))
+                .join("tests")
+                .join("data")
+                .join(IMAGE_TO_LOAD),
+        )
+        .unwrap()
+        .resize(
+            input0_shape[2] as u32,
+            input0_shape[3] as u32,
+            FilterType::Nearest,
+        )
+        .to_luma();
+
+        let array = ndarray::Array::from_shape_fn((1, 1, 28, 28), |(_, c, j, i)| {
+            let pixel = image_buffer.get_pixel(i as u32, j as u32);
+            let channels = pixel.channels();
+
+            // range [0, 255] -> range [0, 1]
+            (channels[c] as f32) / 255.0
+        });
+
+        // Batch of 1
+        let input_tensor_values = vec![array];
+
+        // Perform the inference
+        let outputs: Vec<
+            onnxruntime::tensor::OrtOwnedTensor<f32, ndarray::Dim<ndarray::IxDynImpl>>,
+        > = session.run(input_tensor_values).unwrap();
+
+        let mut probabilities: Vec<(usize, f32)> = outputs[0]
+            .softmax(ndarray::Axis(1))
+            .into_iter()
+            .copied()
+            .enumerate()
+            .collect::<Vec<_>>();
+
+        // Sort probabilities so highest is at beginning of vector.
+        probabilities.sort_unstable_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+
+        assert_eq!(
+            probabilities[0].0, 5,
+            "Expecting class for {} is '5' (not {})",
+            IMAGE_TO_LOAD, probabilities[0].0
+        );
+    }
 }
 
 fn get_imagenet_labels() -> Result<Vec<String>, io::Error> {
