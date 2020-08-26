bfb4a5fde7225b558ad9b95ac4d78a2f531701ff
Nicolas Bigaouette <nbigaouette@gmail.com>
2020-08-26 14:43:44 -0400

    Bump ONNX Runtime version to 1.4.0

--- onnxruntime-sys/build.rs
+++ onnxruntime-sys/build.rs
@@ -6,7 +6,7 @@ use std::{
 };
 
 /// ONNX Runtime version
-const ORT_VERSION: &str = "1.3.0";
+const ORT_VERSION: &str = "1.4.0";
 
 /// Base Url from which to download pre-built releases/
 const ORT_RELEASE_BASE_URL: &str = "https://github.com/microsoft/onnxruntime/releases/download";
