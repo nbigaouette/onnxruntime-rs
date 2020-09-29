124578ed6e8f1c07be958150c234e5a1a01afcaf
Nicolas Bigaouette <nbigaouette@gmail.com>
2020-09-29 19:52:10 -0400

    Set tracing debug level to `trace`

--- onnxruntime/examples/issue22.rs
+++ onnxruntime/examples/issue22.rs
@@ -5,8 +5,20 @@
 
 use ndarray::Array2;
 use onnxruntime::{environment::Environment, tensor::OrtOwnedTensor, GraphOptimizationLevel};
+use tracing::Level;
+use tracing_subscriber::FmtSubscriber;
 
 fn main() {
+    // a builder for `FmtSubscriber`.
+    let subscriber = FmtSubscriber::builder()
+        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
+        // will be written to stdout.
+        .with_max_level(Level::TRACE)
+        // completes the builder.
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
     let env = Environment::builder().with_name("env").build().unwrap();
     let mut session = env
         .new_session_builder()
